a2f234cf45bb0d3ba172b8e5d69eab051ab56291
sarahslopes <sarah2@discente.ufg.br>
2022-07-15 14:03:25 -0300

    ex2

---
+++ ex2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int v1, v2;
+
+    if (&v1 > & v2)
+        printf("O maior endereco de memoria é: %p\n", &v1);
+    else(
+        printf("O maior endereco de memoria é: %p\n", &v2)
+    );
+}
+
