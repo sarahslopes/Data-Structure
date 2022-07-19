98687c55a3638e5130c857de5d2e551011925c0a
Sarah Silva Lopes <92373290+sarahslopes@users.noreply.github.com>
2022-07-18 21:02:42 -0300

    conserto ex10.c

--- ex10.c
+++ ex10.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 int i;
+int dobro(int v1[]);
 
 int main() {
     int v1[5], *p;
-    p = &v1;
+    p = v1;
 
     for(i = 0; i < 5; i++){
         scanf("%d", &p[i]);
     }
 
-    dobro(&v1);
+    dobro(p);
 
 }
 
-int dobro(int v1[], int *p){
+int dobro(int v1[]){
     for(i = 0; i < 5; i++){
-        printf("%d", v1[i] * 2);
+        printf("%d ", v1[i] * 2);
     }
 
 }
