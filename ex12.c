3ebe9dc3fc4db8b0282bf5b5f8df046e6418271f
Sarah Silva Lopes <92373290+sarahslopes@users.noreply.github.com>
2022-07-18 21:12:19 -0300

    Fix ex12

--- ex12.c
+++ ex12.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int i = 0;
-
-int main(){
-    int a, b, c, *pa, *pb, *pc;
-    pa = &a;
-    pb = &b;
-    pc = &c;
-
-    scanf("%d%d%d", &a, &b, &c);
-
-    verifica(&a, &b, &c);
-
-     printf("%d %d %d", *pa, *pb, *pc);
-
-}
 
 int verifica(int *pa, int *pb, int *pc){
 
@@ -38,7 +25,7 @@ int verifica(int *pa, int *pb, int *pc){
             *pb = tmp;
         }
 
-            if(*pa == *pb == *pc)
+        if(*pa == *pb == *pc)
             return 1;
         else
             return 0;
@@ -46,4 +33,18 @@ int verifica(int *pa, int *pb, int *pc){
 
     }
 
+}
+
+int main(){
+    int a, b, c, *pa, *pb, *pc;
+    pa = &a;
+    pb = &b;
+    pc = &c;
+
+    scanf("%d%d%d", &a, &b, &c);
+
+    int response = verifica(&a, &b, &c);
+
+    printf("%d %d %d", *pa, *pb, *pc);
+    exit(response);
 }
